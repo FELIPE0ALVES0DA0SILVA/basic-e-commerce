3469a9c2c1ea3affabada3e5afbe75aa634d7848
FELIPE0ALVES0DA0SILVA <felipe2018cannavarro@gmail.com>
2022-03-28 09:08:51 -0300

    0.1 ADD Annoucement Category CAtegoryItem NavBar Slider Home: Created components and styled it with styled-components and material-ui

---
+++ client/src/data.js
@@ -0,0 +1,64 @@
+export const sliderItems = [
+  {
+    id: 0,
+    img: 'image-slider.jpg',
+    title: 'SUMMER SALE',
+    description: 'DONT COMPROMISE ON STYELE! GET FLAT 30% OFF BRO',
+    bg: 'f5fafd'
+  },
+  {
+    id: 1,
+    img: 'image-slider.jpg',
+    title: 'MACARRAO SALE',
+    description: 'DONT COMPROMISE ON STYELE! GET FLAT 30% OFF BRO',
+    bg: 'fcf1ed'
+  },
+  {
+    id: 2,
+    img: 'image-slider.jpg',
+    title: 'FEIJOADA SALE',
+    description: 'DONT COMPROMISE ON STYELE! GET FLAT 30% OFF BRO',
+    bg: 'ffbf0f4'
+  },
+]
+
+export const categoryItems = [
+  {
+    id: 0,
+    img: 'image-slider.jpg',
+    title: 'SHIRT STYLE',
+  },
+  {
+    id: 0,
+    img: 'image-slider.jpg',
+    title: 'SHIRT STYLE',
+  },
+  {
+    id: 0,
+    img: 'image-slider.jpg',
+    title: 'SHIRT STYLE',
+  },
+]
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: '',
+  }
+  {
+    id: 2,
+    img: '',
+  }
+  {
+    id: 3,
+    img: '',
+  }
+  {
+    id: 4,
+    img: '',
+  }
+  {
+    id: 5,
+    img: '',
+  }
+]
